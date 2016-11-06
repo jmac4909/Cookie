0383601cd17fa45432fb7a082726cd241c675e1a
Jeremy Mackey <jmackey@Jeremys-MacBook-Pro.local>
2016-11-06 16:02:32 -0500

    - Added level 4

---
+++ JMLevel03.h
@@ -0,0 +1,15 @@
+//
+//  JMLevel03.h
+//  cookie
+//
+//  Created by Jeremy Mackey on 11/6/16.
+//  Copyright © 2016 Jeremy Mackey. All rights reserved.
+//
+
+#import "JMBasicLevel.h"
+#import "JMLevelSelect.h"
+#import "JMLevel02.h"
+
+@interface JMLevel03 : JMBasicLevel
+
+@end
